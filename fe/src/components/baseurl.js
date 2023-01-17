e09ed5bb9518b6d461c16e5d3bbd36fbf9a9ddc3
hilalmustofa <hilal.mustofa90@gmail.com>
2023-01-17 12:51:02 +0700

    remove fix baseurl

--- fe/src/components/baseurl.js
+++ fe/src/components/baseurl.js
@@ -1,4 +1,4 @@
 const baseUrl = process.env.NODE_ENV === "production"
-    ? "https://shopuwuh.herokuapp.com"
-    : "http://localhost:80";
+    ? "http://akuntesterwork-001-site1.ctempurl.com"
+    : "http://akuntesterwork-001-site1.ctempurl.com";
 export default baseUrl;

f56e11506340c0a3789be973a4c006d5236a6979
hilalmustofa <hilal.mustofa90@gmail.com>
2023-01-19 13:38:08 +0700

    change environment

--- fe/src/components/baseurl.js
+++ fe/src/components/baseurl.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NODE_ENV === "production"
+const baseUrl = process.env.NODE_ENV === "development"
     ? "http://akuntesterwork-001-site1.ctempurl.com"
     : "http://akuntesterwork-001-site1.ctempurl.com";
 export default baseUrl;

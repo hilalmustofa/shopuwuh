ade3b39de1a1962876f82ab5c7c1f99e7e285f7f
hilalmustofa <hilal.mustofa90@gmail.com>
2023-01-19 13:29:09 +0700

    change baseUrl

--- fe/src/components/baseurl.js
+++ fe/src/components/baseurl.js
@@ -1,4 +1,4 @@
 const baseUrl = process.env.NODE_ENV === "production"
-    ? "https://average-ruby-narwhal.cyclic.app"
-    : "https://average-ruby-narwhal.cyclic.app";
+    ? "http://akuntesterwork-001-site1.ctempurl.com"
+    : "http://akuntesterwork-001-site1.ctempurl.com";
 export default baseUrl;
